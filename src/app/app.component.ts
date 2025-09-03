1aa69fb86875f0c1f2566fd79ba3a858bb102baf
aaditya890 <mishraaaditya297@gmail.com>
2025-09-03 16:33:12 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, signal, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ContactUsComponent } from "./components/contact-us/contact-us.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'the-girls-room';
+  @ViewChild('navbar') navbarRef!: ElementRef<HTMLDivElement>;
+  isSticky = false;
+  navHeight = 80;
+  topbarHeight = 32;
+  heroUrl = 'assets/hero-bg.jpg';
+
+  ngAfterViewInit(): void {
+    queueMicrotask(() => {
+      if (this.navbarRef?.nativeElement) this.navHeight = this.navbarRef.nativeElement.offsetHeight || this.navHeight;
+      this.onScroll();
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const y = window.scrollY || window.pageYOffset;
+    this.isSticky = y > 10;
+  }
+  
 }
