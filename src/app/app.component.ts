87251c1b7edcc4a458c7cd4d92f7095eb24695ba
aaditya890 <mishraaaditya297@gmail.com>
2025-09-10 11:54:36 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,28 @@
 import { CommonModule} from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FooterComponent } from "./components/footer/footer.component";
 import { NavComponent } from "./components/nav/nav.component";
 import { RouterOutlet } from '@angular/router';
-
+import {MatDialog, MatDialogModule,} from '@angular/material/dialog';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,FooterComponent, NavComponent,RouterOutlet],
+  imports: [CommonModule,FooterComponent, NavComponent,RouterOutlet,MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'The Girls Room';
+export class AppComponent implements OnInit{
+  title = 'The Girls Room'; 
+
+  constructor(private MatDialog:MatDialog){} 
+
+  ngOnInit(): void {
+    console.log("AppComponent initialized......");
+  }
+
+  openDialog(){
+    this.MatDialog.open(DialogComponent)
+  }
 }
